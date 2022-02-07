3695cbf2244e44677be0c0fd5591bf169624b02e
nikiyyy <wow_niko@protonmail.com>
2022-02-07 18:02:35 +0200

    test

--- refresher/spiders/topMovies.py
+++ refresher/spiders/topMovies.py
@@ -14,3 +14,4 @@ class TopmoviesSpider(scrapy.Spider):
     def parse_movie(self, response):
         #response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
         pass
+#change
