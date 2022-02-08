cc3aaa220bb9bea7d60fb1044a9faf6ff288e8e3
nikiyyy <wow_niko@protonmail.com>
2022-02-08 17:02:34 +0200

    all urs

--- refresher/spiders/topMovies.py
+++ refresher/spiders/topMovies.py
@@ -1,18 +1,26 @@
+from gc import callbacks
+from urllib.request import Request
 import scrapy
 
 
 class TopmoviesSpider(scrapy.Spider):
     name = 'topMovies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
 
     def parse(self, response):
-
-        for i in  response.css('td.titleColumn a::attr(href)').getall():
-            yield response.follow('http://imdb.com' + i, callback = self.parse)
+        for i in response.css('td.titleColumn a::attr(href)').getall():
+            print(i)
+            #yield scrapy.Request(str('http://imdb.com' + i), callback=self.movie_parse)
         
-    def parse_movie(self, response):
+    def movie_parse(self, response):
+        print(1)
+
+
+
+        #response.follow('http://imdb.com' + i, callback = self.parse)
+    #def parse_movie(self, response):
         #response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
-        pass
+        
 #change
 #changePC
