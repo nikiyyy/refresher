a1656818ba55725820575c26afa7bb3180fb09bc
nikiyyy <wow_niko@protonmail.com>
2022-02-09 09:52:17 +0200

    director-year-score

--- refresher/spiders/topMovies.py
+++ refresher/spiders/topMovies.py
@@ -10,14 +10,15 @@ class TopmoviesSpider(scrapy.Spider):
     count=1
     def parse(self, response):
         for i in response.css('td.titleColumn a::attr(href)').getall():
-            yield scrapy.Request('http://imdb.com' + i, callback=self.movie_parse)
+            if self.count <2:
+                self.count+=1
+                yield scrapy.Request('http://imdb.com' + i, callback=self.movie_parse)
 
         
     def movie_parse(self, response):
         yield {
-            "Title" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1/text()').get()
+            "Title" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1/text()').get(),
+            "Director" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get(),
+            "Year" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/div[2]/ul/li[1]/a/text()').get(),
+            "Score" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[2]/div/div[1]/a/div/div/div[2]/div[1]/span[1]/text()').get()
         }
-
-#change
-#changePCgit
-#change from branch
