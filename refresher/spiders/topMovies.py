3a8db6aedbd860c720e3bc6746f2b6495983083f
nikiyyy <wow_niko@protonmail.com>
2022-02-09 09:16:51 +0200

    crawler base

--- refresher/spiders/topMovies.py
+++ refresher/spiders/topMovies.py
@@ -7,21 +7,17 @@ class TopmoviesSpider(scrapy.Spider):
     name = 'topMovies'
     allowed_domains = ['imdb.com']
     start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
-
+    count=1
     def parse(self, response):
         for i in response.css('td.titleColumn a::attr(href)').getall():
-            print(i)
-            #yield scrapy.Request(str('http://imdb.com' + i), callback=self.movie_parse)
+            yield scrapy.Request('http://imdb.com' + i, callback=self.movie_parse)
+
         
     def movie_parse(self, response):
-        print(1)
+        yield {
+            "Title" : response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[1]/div[1]/h1/text()').get()
+        }
 
-
-
-        #response.follow('http://imdb.com' + i, callback = self.parse)
-    #def parse_movie(self, response):
-        #response.xpath('//*[@id="__next"]/main/div/section[1]/section/div[3]/section/section/div[3]/div[2]/div[1]/div[3]/ul/li[1]/div/ul/li/a/text()').get()
-        
 #change
 #changePCgit
 #change from branch
